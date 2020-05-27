6a1a3d217c10ab3279f95020b687eb151e1ea807
Jason <jasonluxe8719@gmail.com>
2020-05-26 19:36:31 -0500

    Rails Lite

---
+++ W8D2/skeleton/lib/controller_base.rb
@@ -0,0 +1,53 @@
+require 'active_support'
+require 'active_support/core_ext'
+require 'erb'
+require_relative './session'
+
+class ControllerBase
+  attr_reader :req, :res, :params
+
+  # Setup the controller
+  def initialize(req, res)
+    @req = res
+    @res = res
+  end
+
+  # Helper method to alias @already_built_response
+  def already_built_response?
+  end
+
+  # Set the response status code and header
+  def redirect_to(url)
+    @res.status = 302
+    @res['Location'] = url
+    nil
+  end
+
+  # Populate the response with content.
+  # Set the response's content type to the given type.
+  # Raise an error if the developer tries to double render.
+  def render_content(content, content_type = "text/html")
+    @res.write(content)
+    @res['Content-Type'] = content_type
+    nil
+  end
+
+  # use ERB and binding to evaluate templates
+  # pass the rendered html to render_content
+  def render(template_name)
+    dir_path = File.dirname(__FILE__)
+    template_path = File.join(dir_path, 'views', "#{template_name}.html.erb")
+    template_code = File.read(template_path)
+    content = ERB.new(template_code).result(binding)
+    render_content(content)
+  end
+
+  # method exposing a `Session` object
+  def session
+  end
+
+  # use this with the router to call action_name (:index, :show, :create...)
+  def invoke_action(name)
+  end
+end
+
